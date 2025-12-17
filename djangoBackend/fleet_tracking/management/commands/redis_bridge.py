8eec29f2b58abf7b8760e6a47f05a2c84ac5c0cc
Ifmai <alpozdemir044@gmail.com>
2025-12-17 02:18:16 +0000

    feat: artık sadece tekli araç datası çekiyoruz. toplu gönderme iptal edilid çok mantıklıda değil bu yüzden ona uygun olan pub delete

--- djangoBackend/fleet_tracking/management/commands/redis_bridge.py
+++ djangoBackend/fleet_tracking/management/commands/redis_bridge.py
@@ -6,7 +6,6 @@ from django.core.management.base import BaseCommand
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
-
 class Command(BaseCommand):
     help = "Redis Pub/Sub -> Django Channels bridge"
 
@@ -17,9 +16,7 @@ class Command(BaseCommand):
         r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
         pubsub = r.pubsub()
         pubsub.psubscribe("fleet.telemetry*") # fleet.telemetry ve fleet.telemetry.<vehicle_id>
-
         channel_layer = get_channel_layer()
-
         self.stdout.write(self.style.SUCCESS(
             f"Bridge başladı. Redis: {redis_host}:{redis_port} | Pattern: fleet.telemetry*"
         ))
@@ -27,26 +24,15 @@ class Command(BaseCommand):
         for msg in pubsub.listen():
             if msg.get("type") != "pmessage":
                 continue
-
             redis_channel = msg["channel"]
-            print("channel name: ", redis_channel)
             payload = msg["data"]
             try:
                 data = json.loads(payload)
             except Exception:
                 data = {"raw": payload}
-
-            # 1) fleet.telemetry  => broadcast
-            print("gelen data:", data)
-            if redis_channel == "fleet.telemetry":
-                group_name = "fleet_all"
-            else:
-                # 2) fleet.telemetry.<vehicle_id> => araca özel
-                parts = redis_channel.split(".")
-                vehicle_id = parts[-1] if len(parts) >= 3 else "unknown"
-                group_name = f"telemetry_vehicle_{vehicle_id}"
-
-            print("group_name_last: ", group_name)
+            parts = redis_channel.split(".")
+            vehicle_id = parts[-1] if len(parts) >= 3 else "unknown"
+            group_name = f"telemetry_vehicle_{vehicle_id}"
             async_to_sync(channel_layer.group_send)(
                 group_name,
                 {"type": "telemetry.message", "data": data}
--- mqtt-worker/telemetry.py
+++ mqtt-worker/telemetry.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 
 WORKER_HOST = os.getenv("MQTT_HOST", "sutlac-mosquitto")
 WORKER_PORT = int(os.getenv("MQTT_PORT", "1883"))
-SUB_TOPIC = "fleet/TR34/vehicle/VEH-0001/telemetry"
+SUB_TOPIC = "fleet/TR34/vehicle/+/telemetry"
 REDIS_HOST = os.getenv("REDIS_HOST", "sutlac-redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_DB   = int(os.getenv("REDIS_DB", "0"))
@@ -21,7 +21,6 @@ def publish_telemetry(payload: dict):
     """
     vehicle_id = payload.get("vehicle_id", "unknown")
     msg = json.dumps(payload, ensure_ascii=False)
-    r.publish("fleet.telemetry", msg)
     r.publish(f"fleet.telemetry.{vehicle_id}", msg)
 
 def on_connect(client, userdata, flags, rc):
