d196fcfaf74fdf2bf39f86076428dc9d9e69efbd
Ifmai <alpozdemir044@gmail.com>
2025-12-17 01:15:50 +0000

    refactor: urls başına api/ eklendi

--- djangoBackend/djangoBackend/urls.py
+++ djangoBackend/djangoBackend/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("feel-tracking/", include("fleet_tracking.Api.urls")),
+    path("api/feel-tracking/", include("fleet_tracking.Api.urls")),
 ]
 
